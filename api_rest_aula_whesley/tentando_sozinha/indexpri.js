1f31996209ffc7b38671af0d2f05522e7ccb69db
Pricilla Souza <pricilla_souzacs@hotmail.com>
2020-04-03 01:38:23 -0300

    subindo exercicio api atualizado

--- api_rest_aula_whesley/tentando_sozinha/indexpri.js
+++ api_rest_aula_whesley/tentando_sozinha/indexpri.js
@@ -6,28 +6,26 @@ const app = express();
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-let listaFuncionarios = fs.readFileSync('./funcionarios.json');
+let listaFuncionarios =  fs.readFileSync('./funcionarios.json');
 let funcio = JSON.parse(listaFuncionarios);
 
 
-app.get('/funcionario', (req, res) => {
+app.get('/funcionario', (req, res)=>{
     res.json(funcio);
 });
 
-
-
-app.post('/funcionario', (req, res) => {
+app.post('/funcionario', (req,res) =>{
     const funcionario = req.body;
-    if (Array.isArray(funcionario)) {
-        for (item of funcionario) {
+    if(Array.isArray(funcionario)){
+        for (item of funcionario){
             funcio.push(item);
         }
     }
-    else {
+    else{
         funcio.push(funcionario);
     }
     let jsonList = JSON.stringify(funcio);
-    fs.writeFile('funcionarios.json', jsonList, 'utf8', () => { });
+    fs.writeFile('funcionarios.json', jsonList, 'utf8', ()=>{});
     res.send('Novo funcionario cadastrado com sucesso!');
 });
 
@@ -45,26 +43,18 @@ app.post('/funcionario', (req, res) => {
 //     res.status(404).send('Funcionario não encontrado!');
 // });
 
-app.get('/funcionario/:idade', (req, res) => {
+app.get('/funcionario/:idade',(req,res)=>{
 
     const idade = req.params.idade;
-    let funcionarios = [];
-    for (let funcionario of funcio) {
-        if (funcionario.idade == idade) {
-            funcionarios.push(funcionario);
-            // console.log(funcionarios);
+    for(let funcionario of funcio){
+        if(funcionario.idade == idade){
+
+            res.json(funcionario);
+            return;
         }
     }
 
-    if (funcionarios.length == 0) {
-        res.status(404).send("Funcionário não encontrado!");
-    }
-    res.json(funcionarios);
-    return;
-
+    res.status(404).send('Funcionario não encontrado!');
 });
 
 app.listen(3000);
-
-
-
